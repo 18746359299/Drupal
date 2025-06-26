0c627e356ec6968c90c147a8654a80626e8f4884
wanghongxin2 <hongxin.wang2@pactera.com>
2025-06-26 17:29:05 +0900

    初始提交 next-drupal demo项目

---
+++ integration/node/NodePage.tsx
@@ -0,0 +1,29 @@
+import { Fragment } from 'react'
+
+import { FragmentOf, readFragment } from 'gql.tada'
+import { NodePageFragment } from '@/graphql/fragments/node'
+import { resolveComponents } from '@/integration/resolvers/components'
+
+type NodePageComponentProps = {
+  node: FragmentOf<typeof NodePageFragment>
+  environment: string
+}
+
+export default function NodePageComponent({ node }: NodePageComponentProps) {
+  const { title, showTitle, components } = readFragment(NodePageFragment, node)
+  // @ts-expect-error skip validation.
+  const resolvedComponents = resolveComponents({ components })
+
+  return (
+    <>
+      {showTitle && (
+        <h1 className="container font-bold mx-auto text-4xl text-gray-900 sm:text-5xl md:text-6xl dark:text-gray-100">
+          {title}
+        </h1>
+      )}
+      {resolvedComponents.map((component, index: number) => {
+        return <Fragment key={index}>{component}</Fragment>
+      })}
+    </>
+  )
+}
